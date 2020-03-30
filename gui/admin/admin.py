7143ac45886d39506bb240f012e119073800258e
Alexis LEBEL <lebalexis@gmail.com>
2020-03-30 16:22:34 +0200

    [GUI] Templates for Admin interface

---
+++ gui/admin/admin.py
@@ -0,0 +1,12 @@
+#
+# Copyright (c) 2020 by Alexis LEBEL, BOUDRY Hugo and PEDROSA Théo. All Rights Reserved.
+# This is the model. There, we will do all the computations needed by the controller,
+# and send back data to the software in itself.
+#
+import admincontroller
+
+class Admin():
+
+    def __init__(self):
+
+        return None
---
+++ gui/admin/admincontroller.py
@@ -0,0 +1,14 @@
+#
+# Copyright (c) 2020 by Alexis LEBEL, BOUDRY Hugo and PEDROSA Théo. All Rights Reserved.
+# This is the controller. There, we will gather the events from the view, and apply
+# the actions related to those events. That class also gather and send data to and from
+# the model, who does the computation and the link with the rest of the software.
+#
+import adminview
+import admin
+
+class Admincontroller():
+
+    def __init__(self):
+
+        return None
