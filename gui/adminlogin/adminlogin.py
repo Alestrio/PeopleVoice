c19889fd8c3f6f135f25c3c875809439337464dd
Alexis LEBEL <lebalexis@gmail.com>
2020-04-03 13:17:54 +0200

    [AdminLogin] Fixed some errors

--- gui/adminlogin/adminlogin.py
+++ gui/adminlogin/adminlogin.py
@@ -11,17 +11,16 @@ class Adminlogin():
         self.window = Tk()
         typePWLabel = Label(self.window, text="Veuillez entrer votre mot de passe ci-dessous:")*
         typePWLabel.pack()
-        self.pw = StringVar()
-        pwTextField = Entry(self.window, textvariable=self.pw, width=30)
-        pwTextField.pack()
+        self.pwTextField = Entry(self.window, width=30)
+        self.pwTextField.pack()
         validateBtn = Button(self.window, text="Valider", command=self.validate)
         validateBtn.pack()
         self.window.mainloop()
         return None
 
     def validate() -> bool:
-        password = self.pw
-        bhashedUserEntry = hashlib.md5(encode(password))
+        password = self.pwTextField.get()
+        bhashedUserEntry = hashlib.md5(password.encode())
         hashedUserEntry = bhashedUserEntry.hexdigest()
         if hashedUserEntry == self.hash:
             self.window.quit()
